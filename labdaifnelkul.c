4061fbe57475eb975336802e9e8b1fb74f5a93cc
kzoltan99 <kzotya99@gmail.com>
2019-05-15 16:54:44 +0200

    Update labdaifnelkul.c

--- labdaifnelkul.c
+++ labdaifnelkul.c
@@ -1,67 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <curses.h>
+#include <unistd.h>
 
-void draw(char v[20][46], int x, int y, char c);
-int main()
+int
+main (void)
 {
-	char v[20][46] ={"#############################################"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#                                           #"
-					,"#############################################"};
-		
-	
-	int xlen=45-1; 
-	int ylen=20-1;		
-	
-	int i=1; 
-	
-	
-	
-	int x=1,y=1; 
-	while(1)
-	{
-		x=abs(xlen-i%(2*xlen)); 
-		y=abs(ylen-i%(2*ylen)); 
-		char v2[20][46];
-		for(int i=0;i<20;i++)
-		{
-			for(int j=0;j<46;j++)
-			{
-				v2[i][j]=v[i][j];
-			}
-		}
-		draw(v2,x,y,'@');
-		i++;
-		usleep(50000);
-	}
-	return 0;
-}
+    int xj = 0, xk = 0, yj = 0, yk = 0;
+    int mx, my;
 
+    WINDOW *ablak;
+    ablak = initscr ();
+    noecho ();
+    cbreak ();
+    nodelay (ablak, true);
 
+    for (;;)
+    {
+	getmaxyx(ablak, my, mx);
+        xj = (xj - 1) % mx;
+        xk = (xk + 1) % mx;
 
-void draw(char v[20][46], int x, int y, char c) 
-{
-	system("clear");
-	v[y][x]=c;
-	for(int i=0;i<20;i++)
-	{
-		printf("%s\n",v[i]);
-	}
-	
-	
+        yj = (yj - 1) % my;
+        yk = (yk + 1) % my;
+
+        //clear ();
+
+        mvprintw (abs (yj + (my - yk)),
+                  abs (xj + (mx - xk)), "X");
+
+        refresh ();
+        usleep (150000);
+
+    }
+    return 0;
 }
